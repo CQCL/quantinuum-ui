79ea882e31ad85b6bf86ce36b9128222f049a0d4
aidanCQ <aidan.keay@quantinuum.com>
2023-12-27 23:33:23 +0000

    fix(): Add use client directive to nav menu,

--- src/atoms/navigation-menu.tsx
+++ src/atoms/navigation-menu.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { ChevronDownIcon } from "@radix-ui/react-icons";
 import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
 import { cva } from "class-variance-authority";
@@ -129,5 +130,6 @@ export {
   NavigationMenuList,
   NavigationMenuTrigger,
   NavigationMenuViewport,
-  navigationMenuTriggerStyle,
+  navigationMenuTriggerStyle
 };
+
