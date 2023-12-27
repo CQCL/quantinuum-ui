07588d1246955cf64e1b7bafd7d53304088383fa
aidanCQ <aidan.keay@quantinuum.com>
2023-12-27 23:40:19 +0000

    fix(): Undo add use client directive to nav menu,

--- src/atoms/navigation-menu.tsx
+++ src/atoms/navigation-menu.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { ChevronDownIcon } from "@radix-ui/react-icons";
 import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
 import { cva } from "class-variance-authority";
