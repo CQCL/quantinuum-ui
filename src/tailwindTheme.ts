932e54468f33c00e88037fb9052e71acc64404cb
aidanCQ <aidan.keay@quantinuum.com>
2024-01-02 23:14:24 +0000

    fix(): XL border radius.

--- src/tailwindTheme.ts
+++ src/tailwindTheme.ts
@@ -48,6 +48,7 @@ export const tailwindTheme = {
         },
       },
       borderRadius: {
+        xl: `calc(var(--radius) + 4px)`,
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
