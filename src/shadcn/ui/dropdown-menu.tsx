c397e43921c332ee150aa190e4769366d16d2c19
aidanCQ <aidan.keay@quantinuum.com>
2024-01-31 12:29:13 +0000

    fix(): Remove default portal.

--- src/shadcn/ui/dropdown-menu.tsx
+++ src/shadcn/ui/dropdown-menu.tsx
@@ -64,7 +64,7 @@ const DropdownMenuContent = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
 >(({ className, sideOffset = 4, ...props }, ref) => (
-  <DropdownMenuPrimitive.Portal>
+
     <DropdownMenuPrimitive.Content
       ref={ref}
       sideOffset={sideOffset}
@@ -75,7 +75,7 @@ const DropdownMenuContent = React.forwardRef<
       )}
       {...props}
     />
-  </DropdownMenuPrimitive.Portal>
+
 ))
 DropdownMenuContent.displayName = DropdownMenuPrimitive.Content.displayName
 
