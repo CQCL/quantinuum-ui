def25018e8ab35a178acf7eccf92ec85e8c269cc
aidanCQ <aidan.keay@quantinuum.com>
2024-02-23 17:19:58 +0000

    feat(): Sonner component.

---
+++ stories/shadcn/sonner.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+
+import { Button, sonner } from "src";
+
+const SonnerDemo = () => {
+    return (
+        <div>
+           
+     
+        <sonner.Toaster >
+
+        </sonner.Toaster>
+         <Button onClick={() => sonner.toast.success('Success!')}>Show Toast</Button>   </div>
+    );
+}
+
+const meta: Meta<typeof SonnerDemo> = {
+    component: SonnerDemo,
+  };
+  
+  export default meta;
+  
+  export const Default: StoryObj<typeof SonnerDemo> = {
+    args: {},
+  };
+  
