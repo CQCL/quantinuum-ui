7cc802244addc2e933a4b98c86d56a12275c4e7a
aidanCQ <aidan.keay@quantinuum.com>
2024-02-26 10:47:41 +0000

    fix(): Add sonner dev dependency and fix story.

--- stories/shadcn/sonner.stories.tsx
+++ stories/shadcn/sonner.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 import { Button, SonnerToast } from "src";
-import sonner from 'sonner'
+import * as sonner from 'sonner'
 const SonnerDemo = () => {
     return (
         <div>
