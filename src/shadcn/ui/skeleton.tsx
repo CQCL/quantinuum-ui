9471fa26e25a143d3a870889b0b4617288c9c69c
aidanCQ <aidan.keay@quantinuum.com>
2024-03-27 17:55:02 +0000

    feat(): Add skeleton accessibility attributes.

--- src/shadcn/ui/skeleton.tsx
+++ src/shadcn/ui/skeleton.tsx
@@ -7,6 +7,11 @@ function Skeleton({
   return (
     <div
       className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      aria-busy="true"
+      role="progressbar"
+      aria-valuemin={0}
+      aria-valuemax={100}
+      aria-valuetext="Please wait..."
       {...props}
     />
   )
