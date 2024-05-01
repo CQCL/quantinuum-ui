3a841ef861a2fd3aad61475d255f1eaa98a1516e
Aidan <104831665+aidanCQ@users.noreply.github.com>
2024-05-01 14:26:07 +0100

    feat(): Multi select component. (#2)
    * Multi select component.
    
    * Update stories/shadcn/multi-select.stories.tsx
    
    ---------
    
    Co-authored-by: aidanCQ <aidan.keay@quantinuum.com>
    Co-authored-by: Sam Clark <20407539+Codar97@users.noreply.github.com>

---
+++ stories/shadcn/multi-select.stories.tsx
@@ -0,0 +1,61 @@
+"use client"
+import type { Meta, StoryObj } from "@storybook/react";
+import React, { ComponentProps } from "react";
+
+import {
+  MultiSelect,
+  MultiSelectContent,
+  MultiSelectEmpty,
+  MultiSelectGroup,
+  MultiSelectItem,
+  MultiSelectList,
+  MultiSelectSearch,
+  MultiSelectTrigger,
+  MultiSelectValue,
+} from "src"
+
+
+
+const Demo = (args: ComponentProps<typeof MultiSelect>) => {
+  const [value, setValue] = React.useState(args.value);
+  return (
+    <MultiSelect onSearch={(v) => {
+        setValue(v)
+    }}>
+    <MultiSelectTrigger className="w-96">
+      <MultiSelectValue placeholder="Select stack" />
+    </MultiSelectTrigger>
+    <MultiSelectContent>
+    <MultiSelectSearch placeholder="input any..." />
+      <MultiSelectList>
+        <MultiSelectGroup heading="React">
+          <MultiSelectItem value="react">React</MultiSelectItem>
+          <MultiSelectItem value="next">Next.js</MultiSelectItem>
+          <MultiSelectItem value="remix">Remix</MultiSelectItem>
+        </MultiSelectGroup>
+        <MultiSelectGroup heading="Vue">
+          <MultiSelectItem value="vue">Vue</MultiSelectItem>
+          <MultiSelectItem value="nuxt">Nuxt</MultiSelectItem>
+        </MultiSelectGroup>
+        <MultiSelectGroup heading="Others">
+          <MultiSelectItem value="angular">Angular</MultiSelectItem>
+          <MultiSelectItem value="svelte">Svelte</MultiSelectItem>
+        </MultiSelectGroup>
+        <MultiSelectEmpty>
+            {"No results found"}
+          </MultiSelectEmpty>
+      </MultiSelectList>
+    </MultiSelectContent>
+  </MultiSelect>
+  );
+};
+
+const meta: Meta<typeof Demo> = {
+  component: Demo,
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof Demo> = {
+  args: {},
+};
