6a43cec8d25eac7f76648fcb7ce7fe1938472ce8
aidanCQ <aidan.keay@quantinuum.com>
2024-09-09 14:57:48 +0100

    fix(): Remove config files.

--- release.config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  branches: ['+([0-9])?(.{+([0-9]),x}).x', 'master', 'main', 'next', 'next-major', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
-  plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
-};
