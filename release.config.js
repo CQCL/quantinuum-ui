dbe40b5f3c2fb93ccccc03738a6d4c975ac7ae57
aidanCQ <aidan.keay@quantinuum.com>
2024-09-09 14:51:47 +0100

    Update default config.

--- release.config.js
+++ release.config.js
@@ -1,4 +1,4 @@
 export default {
-  branches: ["main", {name: 'beta', prerelease: true}],
-  plugins: ["@semantic-release/npm"]
+  branches: ['+([0-9])?(.{+([0-9]),x}).x', 'master', 'main', 'next', 'next-major', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
+  plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
 };
