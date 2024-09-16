63d30311c11041ea4fdd8d678767aebfb9f01989
aidanCQ <aidan.keay@quantinuum.com>
2024-09-16 12:28:43 +0100

    feat(): More docs components.

--- src/custom/docs/index.ts
+++ src/custom/docs/index.ts
@@ -1,2 +1,4 @@
 export {Footer} from './components/footer'
 export { NavBar } from './components/navmenu/index'
+export { TripleCard } from './components/triplecard'
+export * from './components/header'
