6806a846948e58f4ee142e22a957100e46b3ec18
aidanCQ <aidan.keay@quantinuum.com>
2024-09-17 14:18:19 +0100

    fix(): Prefix components with 'Docs'.

--- src/custom/docs/index.ts
+++ src/custom/docs/index.ts
@@ -1,5 +1,5 @@
-export {Footer} from './components/footer'
-export { NavBar } from './components/navmenu/index'
-export { TripleCard } from './components/triplecard'
+export {Footer as DocsFooter } from './components/footer'
+export { NavBar as DocsNavBar } from './components/navmenu/index'
+export { TripleCard as DocsTripleCard } from './components/triplecard'
 export * from './components/header'
 export * from './components/page'
