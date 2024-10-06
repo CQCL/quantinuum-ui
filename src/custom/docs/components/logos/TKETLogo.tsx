80a2f530659873e9377fe714b530ead6de1d7a69
Irfan Khan <irfan.khan@quantinuum.com>
2024-10-06 15:47:48 +0100

    add product logos

---
+++ src/custom/docs/components/logos/TKETLogo.tsx
@@ -0,0 +1,5 @@
+import { ComponentProps } from "react"
+
+export const TKETLogo = (props: ComponentProps<'svg'>) => {
+    return <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 100 20" fill="none" {...props}><path d="M72.6998 3.73757V0.0159046H52.7793V20H72.6998V16.2704H56.5089V11.9602H72.6998V8.23857H56.5089V3.73757H72.6998Z" fill="currentColor"></path><path d="M77.0577 3.79324H86.5288V20H90.2664V3.79324H99.7376V0.0636183H77.0577V3.79324Z" fill="currentColor"></path><path d="M0 3.79324H9.47117V20H13.2167V3.79324H22.6879V0.0636183H0V3.79324Z" fill="currentColor"></path><path d="M48.3499 0H42.1392L32.1034 7.52286V0.0795229H28.3817V19.9841H32.1034V12.7157L41.8131 20H48.0159L34.8469 10.1233L48.3499 0Z" fill="currentColor"></path></svg>
+}
