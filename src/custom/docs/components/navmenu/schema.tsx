de8e3c351ee5d0ba96bb9ed6352c065dfa586987
irfankhan10 <irfan.khan@Quantinuum.com>
2024-10-07 10:09:29 +0100

    feat(): :DocsNavBar with DropDown Menu
    * add product logos
    
    * update schema to include types for dropdown menu content
    
    * hard-code content into navmenu/index.tsx
    
    * Navigation component uses primitive components to enable dropdown menu with list of items
    
    * update story for modified navigation menu
    
    * add padding to text within dropdown logo box
    
    * update content for lambeq and inquanto

--- src/custom/docs/components/navmenu/schema.tsx
+++ src/custom/docs/components/navmenu/schema.tsx
@@ -1,16 +1,24 @@
 import { ComponentProps } from 'react'
+import React from 'react';
 import { z } from 'zod';
 
-
+const dropDownSchema = z.object({
+    href: z.string(),
+    title: z.string(), 
+});
 const linkSchema = z.object({
     href: z.string(),
     title: z.string(),
+    logo: z.custom<React.JSX.Element>(
+        e => (e as any)?.$$typeof === Symbol.for("react.element"),
+        "value must be a React Element"
+    ),
+    description: z.string(),
     external: z.boolean().optional(),
+    dropDown: z.array(dropDownSchema),
 });
 export const navConfigSchema = z.object({
     navTextLinks: z.array(linkSchema),
-    navIconLinks: z.array(z.intersection(linkSchema, z.object({iconImageURL: z.string()}))),
-    navProductName: z.string()
 })
 export type NavConfig = z.infer<typeof navConfigSchema>
 export type ActivePaths = (NavConfig['navTextLinks'])[number]['href']
