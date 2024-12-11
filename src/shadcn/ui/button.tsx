a119eebff341b3b8c26a548b61e020f35054e947
aidanCQ <aidan.keay@quantinuum.com>
2024-12-11 17:12:54 +0000

    doc(): Add sidebar story.

--- src/shadcn/ui/button.tsx
+++ src/shadcn/ui/button.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
-
-import { cn } from "src/utils"
-
+ 
+import { cn } from "src"
+ 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
@@ -33,13 +33,13 @@ const buttonVariants = cva(
     },
   }
 )
-
+ 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
-
+ 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
@@ -53,5 +53,5 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   }
 )
 Button.displayName = "Button"
-
+ 
 export { Button, buttonVariants }
---
+++ stories/shadcn/sidebar.stories.tsx
@@ -0,0 +1,380 @@
+
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Box,
+  BoxIcon,
+  ChevronDown,
+  Command,
+  FolderIcon,
+  FoldersIcon,
+  Frame,
+  GalleryVerticalEnd,
+  HammerIcon,
+  Map,
+  PieChart,
+  Settings2,
+  SquareTerminal,
+  TerminalIcon,
+  Zap,
+} from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarProvider,
+  SidebarRail,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  SidebarTrigger,
+  Separator,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuItem,
+  DropdownMenu,
+  DropdownMenuTrigger,
+  SidebarMenuButton,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "src"
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "Playground",
+      url: "#",
+      icon: SquareTerminal,
+      
+      items: [
+        {
+          title: "History",
+          url: "#",
+        },
+        {
+          title: "Starred",
+          url: "#",
+        },
+        {
+          title: "Settings",
+          url: "#",
+        },
+      ],
+    },
+  
+   
+  ],
+  
+}
+
+
+  
+  import type { Meta, StoryObj } from "@storybook/react";
+
+
+  
+
+const SidebarDemo = () => {
+    const data = {
+        user: {
+          name: "shadcn",
+          email: "m@example.com",
+          avatar: "/avatars/shadcn.jpg",
+        },
+        teams: [
+          {
+            name: "Acme Inc",
+            logo: GalleryVerticalEnd,
+            plan: "Enterprise",
+          },
+          {
+            name: "Acme Corp.",
+            logo: AudioWaveform,
+            plan: "Startup",
+          },
+          {
+            name: "Evil Corp.",
+            logo: Command,
+            plan: "Free",
+          },
+        ],
+        navMain: [
+          {
+            title: "folder123",
+            url: "#",
+            icon: FolderIcon,
+            
+            items: [
+              {
+                title: "circuitA",
+                url: "#",
+              },
+              {
+                title: "circuitB",
+                url: "#",
+              },
+              {
+                title: "circuitC",
+                url: "#",
+              },
+            ],
+          },
+          {
+            title: "folder123",
+            url: "#",
+            icon: FolderIcon,
+            
+            items: [
+              {
+                title: "circuitA",
+                url: "#",
+              },
+              {
+                title: "circuitB",
+                url: "#",
+              },
+              {
+                title: "circuitC",
+                url: "#",
+              },
+            ],
+          },
+          {
+            title: "folder123vv",
+            url: "#",
+            icon: FolderIcon,
+            
+            items: [
+              {
+                title: "circuitA",
+                url: "#",
+              },
+              {
+                title: "circuitB",
+                url: "#",
+              },
+              {
+                title: "circuitC",
+                url: "#",
+              },
+            ],
+          },
+          {
+            title: "foldeav",
+            url: "#",
+            icon: FolderIcon,
+            
+            items: [
+              {
+                title: "circuitA",
+                url: "#",
+              },
+              {
+                title: "circuitB",
+                url: "#",
+              },
+              {
+                title: "circuitC",
+                url: "#",
+              },
+            ],
+          },
+          {
+            title: "folder1234",
+            url: "#",
+            icon: FolderIcon,
+            
+            items: [
+              {
+                title: "circuitA",
+                url: "#",
+              },
+              {
+                title: "circuitB",
+                url: "#",
+              },
+              {
+                title: "circuitC",
+                url: "#",
+              },
+            ],
+          },
+         
+        
+        ],
+        jobs: [
+            {
+                title: "All",
+                url: "#",
+                icon: BoxIcon,
+              },
+            {
+              title: "Compile",
+              url: "#",
+              icon: HammerIcon,
+              
+              items: [
+                {
+                  title: "job1",
+                  url: "#",
+                },
+                {
+                  title: "job2",
+                  url: "#",
+                  icon: Box,
+                },
+                {
+                  title: "job3",
+                  url: "#",
+                },
+              ],
+            },
+            {
+              title: "Execute",
+              url: "#",
+              icon: Zap,
+              items: [
+                {
+                  title: "Genesis",
+                  url: "#",
+                },
+                {
+                  title: "Explorer",
+                  url: "#",
+                },
+                {
+                  title: "Quantum",
+                  url: "#",
+                },
+              ],
+            },
+            {
+              title: "QMCI",
+              url: "#",
+              icon: TerminalIcon,
+              items: [
+                {
+                  title: "Introduction",
+                  url: "#",
+                },
+                {
+                  title: "Get Started",
+                  url: "#",
+                },
+                {
+                  title: "Tutorials",
+                  url: "#",
+                },
+                {
+                  title: "Changelog",
+                  url: "#",
+                },
+              ],
+            },
+         
+          ],
+        projects: [
+          {
+            name: "Design Engineering",
+            url: "#",
+            icon: Frame,
+          },
+          {
+            name: "Sales & Marketing",
+            url: "#",
+            icon: PieChart,
+          },
+          {
+            name: "Travel",
+            url: "#",
+            icon: Map,
+          },
+        ],
+      }
+  return (
+    <SidebarProvider>
+    <Sidebar collapsible="icon" >
+      <SidebarHeader>
+    Header
+      </SidebarHeader>
+      <SidebarContent>
+    
+        Content
+      </SidebarContent>
+      <SidebarFooter>
+      <div>Footer</div>
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+    <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+          </div>
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  
+  );
+};
+const meta: Meta<typeof SidebarDemo> = {
+  component: SidebarDemo,
+ 
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof SidebarDemo> = {
+  args: {},
+};
