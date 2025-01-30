59e260a6ae22e3244b95c74d092eaee9c22cdba1
aidanCQ <aidan.keay@quantinuum.com>
2025-01-30 13:44:49 +0000

    fix: Update theme config.

--- src/tailwindTheme.ts
+++ src/tailwindTheme.ts
@@ -18,7 +18,9 @@ export const tailwindTheme = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        paper: "hsl(var(--paper))",
+        paper: {
+          DEFAULT: "hsl(var(--paper))"
+        },
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
