fa93f0e5f4ce35965e13b5d3c680994e96afb100
Aidan <104831665+aidanCQ@users.noreply.github.com>
2025-03-26 11:08:52 +0000

    fix: Update sonner (#53)
    * fix(UI): Update sonner component.
    
    * fix(): Update prelease channel list.
    
    * fix: export name.
    
    * fix(): Export sonner.

--- src/shadcn/ui/sonner.tsx
+++ src/shadcn/ui/sonner.tsx
@@ -1,38 +1,28 @@
 "use client"
 
-import { Toaster as Sonner} from "sonner"
+import { Toaster as Sonner } from "sonner"
 import { cn } from "src/utils"
 export * as sonner from "sonner"
-
 type ToasterProps = React.ComponentProps<typeof Sonner>
 
-export const SonnerToast = ({ theme, className, toastOptions, ...props }: ToasterProps) => {
+const SonnerToast = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
-      className={cn("toaster group", className)}
       toastOptions={{
-        ...toastOptions,
         classNames: {
-          toast: cn(
-              "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-              toastOptions?.classNames?.toast
-          ),
-          description: cn("group-[.toast]:text-muted-foreground", toastOptions?.classNames?.description),
-          actionButton: cn(
+          toast:
+            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          description: "group-[.toast]:text-muted-foreground",
+          actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
-              toastOptions?.classNames?.actionButton
-          ),
-          cancelButton: cn(
-            "group-[.toaster]:bg-muted group-[.toaster]:text-muted-foreground bg-red",
-              toastOptions?.classNames?.cancelButton
-          ),
-          closeButton:cn(
-            "group-[.toaster]:bg-muted group-[.toaster]:border-border group-[.toaster]:text-foreground bg-red group-[.toaster]:hover:text-muted-foreground",
-              toastOptions?.classNames?.closeButton
-          ),
+          cancelButton:
+            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
         },
       }}
       {...props}
+      className={cn("toaster group", props.className)}
     />
   )
 }
+
+export { SonnerToast }
