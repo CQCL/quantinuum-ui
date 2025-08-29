20f3ed601567a01dde4a75ea61bca1b37f2f0876
aidanCQ <aidan.keay@quantinuum.com>
2025-08-29 12:03:33 +0100

    fix: Reinstate some components.

---
+++ stories/shadcn/sheet.stories.tsx
@@ -0,0 +1,37 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { ComponentProps } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "src";
+
+const Demo = (args: ComponentProps<typeof Sheet>) => {
+  return (
+    <Sheet {...args}>
+      <SheetTrigger>Open</SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+const meta: Meta<typeof Sheet> = {
+  component: Demo,
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof Sheet> = {
+  args: {},
+};
