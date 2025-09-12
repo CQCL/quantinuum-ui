7baedd2a55d3f880b78ace83e63a67ff98152a33
Aidan <104831665+aidanCQ@users.noreply.github.com>
2025-09-12 16:53:27 +0100

    fix: Emit ts declaration as one file.

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,10 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import copy from "rollup-plugin-copy";
+import dts from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import preserveDirectives from "rollup-plugin-preserve-directives";
 import { terser } from "rollup-plugin-terser";
-
 export default [{
   onwarn(warning, warn) {
     if (
@@ -31,7 +31,7 @@ export default [{
     commonjs(),
     typescript({
       tsconfig: "./tsconfig.json",
-      declarationDir: "./dist/types",
+      declaration: false,
     }),
     copy({
       targets: [{ src: "./src/tokens.css", dest: "./dist" }],
@@ -40,7 +40,12 @@ export default [{
     preserveDirectives(),
   ],
 
-}, {
+},
+ {
+    input: "src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  }, {
   input: "src/utils/syncTheme.ts",
   output: [
     {
