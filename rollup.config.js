f305a336e5cd12896106f9547983879540b45dca
Aidan <104831665+aidanCQ@users.noreply.github.com>
2025-09-23 09:12:39 +0100

    Move deps.

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,11 @@ import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import copy from "rollup-plugin-copy";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import postcss from 'rollup-plugin-postcss';
 import preserveDirectives from "rollup-plugin-preserve-directives";
 import { terser } from "rollup-plugin-terser";
-export default [{
-  onwarn(warning, warn) {
+
+
+function onwarn(warning, warn) {
     if (
       warning.code === "MODULE_LEVEL_DIRECTIVE" &&
       warning.message.includes(`'use client'`)
@@ -15,7 +15,9 @@ export default [{
       return;
     }
     warn(warning);
-  },
+  }
+export default [{
+  onwarn,
   input: "src/index.ts",
   output: [
     {
@@ -59,16 +61,5 @@ export default [{
     }),
     terser(),
   ],
-
-},
-// Generate small tailwind class manifest for more efficient compiling by consumers.
-{
-  input: ".storybook/styles.css",
-  output: [{ file: "dist/tailwind-manifest.css", format: "es" }],
-  plugins: [
-      postcss({
-          extract: true,
-          minimize: true,
-      }),
-  ],
-},];
+}
+];
